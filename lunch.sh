bf16d0bba2b344d08cb238665356a958babd0213
Raven <craven@crowz.kr>
2017-06-04 00:56:14 -0400

    GDB Debug 를 위한 -s -S 옵션 추가

--- lunch.sh
+++ lunch.sh
@@ -1,6 +1,7 @@
 
 
 cd linux
- qemu-system-arm -M vexpress-a9 -cpu cortex-a9 -m 128M -kernel arch/arm/boot/zImage -initrd initrd -serial stdio -append "root=/dev/ram rdinit=init console=ttyAMA0" -dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb
+ qemu-system-arm -M vexpress-a9 -cpu cortex-a9 -m 128M -kernel arch/arm/boot/zImage -initrd initrd -serial stdio -append "root=/dev/ram rdinit=init console=ttyAMA0" -dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb -s -S
+ #qemu-system-arm -M vexpress-a9 -cpu cortex-a9 -m 128M -kernel arch/arm/boot/zImage -initrd initrd -serial stdio -append "root=/dev/ram rdinit=init console=ttyAMA0" -dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb
 
 
